e36981cc7868121446ac2c6fc58678b84fc62c9c
Gaurav Verma <gverma@operative.com>
2019-08-31 00:12:39 +0530

    4. Added Spring Eureka server

--- company-web-service/src/main/java/com/poc/company/CompanyApplication.java
+++ company-web-service/src/main/java/com/poc/company/CompanyApplication.java
@@ -3,12 +3,14 @@ package com.poc.company;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompanyApplication {
 
 	public static void main(String[] args) {
---
+++ sprint-cloud-eureka-server/src/test/java/com/operative/discovery/OperativeToDoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operative.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperativeToDoEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
