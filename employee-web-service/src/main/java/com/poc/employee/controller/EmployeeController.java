4e05a95b1ca92fd608f7c9d1f81ed577f803b4ae
Gaurav Verma <gverma@operative.com>
2019-08-31 11:33:57 +0530

    Adding swagger documentation .

--- employee-web-service/src/main/java/com/poc/employee/controller/EmployeeController.java
+++ employee-web-service/src/main/java/com/poc/employee/controller/EmployeeController.java
@@ -10,14 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.poc.employee.model.Employee;
 import com.poc.employee.service.EmployeeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 
 @RestController
 @RequestMapping("api/v1/employee")
+@Api(value="Employee Management System", description="Operations pertaining to employee in Employee Management System")
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
    // to calll this service  http://localhost:8081/api/v1/employee/getAll
+	@ApiOperation(value = "View a list of available employees")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	
 	@GetMapping(value = "/getAll")
 	public List<Employee> getAll() {
 
